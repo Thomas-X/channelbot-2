b73a02b4a7dd6026c24e8866d84707b951cbff86
Thomas Zwarts <thomaszwarts@gmail.com>
2019-03-12 00:28:19 +0100

    init jsonconvert database migration project

---
+++ channelbot-2-jsonconvert/DataStructures/ChannelJson.cs
@@ -0,0 +1,14 @@
+﻿namespace channelbot_2_jsonconvert.DataStructures
+{
+    public class ChannelJson
+    {
+        public string channel { get; set; }
+        public string channel_id { get; set; }
+        public string subreddit { get; set; }
+        public string user { get; set; }
+        public int register_date { get; set; }
+        public string upload_playlist { get; set; }
+        public string[] last_videos { get; set; }
+        public int last_check { get; set; }
+    }
+}
--- channelbot-2-jsonconvert/Program.cs
+++ channelbot-2-jsonconvert/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using channelbot_2_jsonconvert.DataStructures;
+using Newtonsoft.Json;
 
 namespace channelbot_2_jsonconvert
 {
@@ -6,6 +10,13 @@ namespace channelbot_2_jsonconvert
     {
         static void Main(string[] args)
         {
+            // TODO Handle invalid PMs better than just plain ignoring it
+            // TODO Step 0: Investigate issue with reddit token expiring
+            // TODO Step 1: Setup channelbot on server
+            // TODO Step 2: Send Reddit PMs to bot with add data (limited to 20 for testing reasons)
+            // TODO Step 3: Validate bot working
+            // TODO HMAC for pubsubhubbub
+            var bla = JsonConvert.DeserializeObject<List<ChannelJson>>(File.ReadAllText("channels.json"));
             Console.WriteLine("Hello World!");
         }
     }
